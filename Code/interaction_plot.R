8e8a2f14e98b261fcb765658f1794b3c33d8fbb2
Benjamin Listyg <listyg.ben@gmail.com>
2021-02-15 14:25:04 -0500

    Update interaction_plot.R

--- Code/interaction_plot.R
+++ Code/interaction_plot.R
@@ -5,32 +5,41 @@ library(extrafont)
 loadfonts(device="win")
 windowsFonts(Times=windowsFont("TT Times New Roman"))
 
-lmer_gma <- lmer_gma %>% rename(`GMA-Job Performance Correlation` = RXY)
+lmer_gma$TenureQuartile <- as.factor(ntile(lmer_gma$Tenure, 4))
 
-# lmer_gma <- lmer_gma %>% data.frame()
+lmer_gmaQ1 <- filter(lmer_gma, TenureQuartile == 1)
+lmer_gmaQ3 <- filter(lmer_gma, TenureQuartile == 3)
 
-model_plot_lmer <- lmer(`GMA-Job Performance Correlation` ~ StudyYear + 
-                     Tenure + 
-                     CriterionType_contrast +
-                     StudyYear:CriterionType_contrast + 
-                     Tenure:CriterionType_contrast +
-                     StudyYear:Tenure +  
-                     (1 | Article), 
-                   data = lmer_gma,
-                   weights = VarE)
+lmer_gma$TenureQuartile <- NULL
 
-plot_data_lmer <- ggpredict(model_plot_lmer, c("StudyYear", "Tenure"))
+mean_line <- ggplot(lmer_gma) +
+  aes(StudyYear,`GMA-Job Performance Correlation`, col = 'JobTenure') +
+  stat_smooth(method = "lm", fullrange = T, se = F, linetype = 'solid', col = 'black')
 
-ggplot(plot_data_lmer, aes(x = x, y = predicted, color = group, linetype = group)) +
-  stat_smooth(method = "lm", se = T, fullrange = F) + 
+mean_data <- mean_line$data
+mean_data$group <- as.factor("Mean")
+
+Q1_data <- stat_smooth(method = "lm", fullrange = T, data = lmer_gmaQ1, se = F, linetype = 'twodash', col = 'gray69')$data
+Q1_data <- rename(Q1_data, 'group'= "TenureQuartile")
+
+Q3_data <- stat_smooth(method = "lm", fullrange = T, data = lmer_gmaQ3, se = F, linetype = 'twodash', col = 'gray69')$data
+Q3_data <- rename(Q3_data, 'group'= "TenureQuartile")
+
+plot_data <- rbind(mean_data, Q1_data, Q3_data)
+
+ggplot(plot_data, aes(x = StudyYear, y = `GMA-Job Performance Correlation`, color = group, linetype = group)) +
+  stat_smooth(method = "lm", se = F, fullrange = T) + 
   ylab('GMA-Job Performance Correlation') + 
   xlab('Study Year')  + 
+  ylim(c(-1,1)) + 
   scale_linetype_manual(name="Job\nTenure",
-                     labels=c("-1 SD","Mean","+1 SD"),
-                     values=c("solid", "twodash", "dashed")) +
+                        labels=c("Mean (805 Days)",expression(paste(1^"st", "Quartile", " (122 Days)")),expression(paste(3^"rd", "Quartile", " (1095 Days)"))),
+                        values=c("solid", "twodash", "dashed")) +
   scale_color_manual(name="Job\nTenure",
-                     labels=c("-1 SD","Mean","+1 SD"),
-                     values=c("gray69","gray42","black")) +
+                     labels=c("Mean (805 Days)",expression(paste(1^"st", "Quartile", " (122 Days)")),expression(paste(3^"rd", "Quartile", " (1095 Days)"))),
+                     values=c("black","gray69","gray42")) +
   scale_x_continuous(breaks=seq(1942, 2018, 19))+ 
   theme_classic(base_family='Times New Roman', 
                 base_size = 20)
+
+  
